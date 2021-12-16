002c6a8bcf4fa57053135cf4440f93fde3187782
polina <92105596+zhitarchuk018@users.noreply.github.com>
2021-12-16 13:56:07 +0000

    rfvhawrf

---
+++ lec_3_my_module.py
@@ -0,0 +1,11 @@
+# модуль lec_3_my_module.py
+
+# атрибуты модуля
+
+a = 3
+b = 'good'
+c = ['bad',4,'koralina molodec']
+
+earth_mass = 5.97237 * 10**30
+sigma_steff_bolc = 5.67036713 * 10**(-8)
+gravity_constant = 6.67408 * 10**(-11)
