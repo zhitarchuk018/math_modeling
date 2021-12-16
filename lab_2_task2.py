1324571cf87f66c464ed67bbbf9370206b56c4aa
polina <92105596+zhitarchuk018@users.noreply.github.com>
2021-12-16 14:02:47 +0000

    tgbhyt

--- lab_2_task2.py
+++ lab_2_task2.py
@@ -1,1 +1,10 @@
-a = input()
+a = 2
+b = 5
+length = 10
+
+g = []
+for n in range(1, length + 1)
+  g.append(a * b**(n-1))
+
+print(g)
+  
---
+++ lab_2_task4.py
@@ -0,0 +1,14 @@
+n = int(input('ВВедите число элементов:'))
+
+s = 0
+a = 1
+b = 1
+print(a)
+print(b)
+
+for i in range(n)
+ s = a + b
+ a = b
+ b = s
+ print(b)
+ 
